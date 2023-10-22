7ad94806bbb0732ccc7fe1aa5c095ac2fb731ade
Mateusz Karbowiak <mtszkarbowiak@gmail.com>
2023-10-22 12:05:55 +0200

    Add coroutine starting and stopping

--- Source/Engine/Scripting/Coroutines/CoroutinesScript.cs
+++ Source/Engine/Scripting/Coroutines/CoroutinesScript.cs
@@ -1,4 +1,7 @@
+#nullable enable
+
 using System;
+using System.Linq;
 
 namespace FlaxEngine;
 
@@ -18,19 +21,78 @@ public class CoroutinesScript : Script
         //TODO(mtszkarbowiak) Implement.
     }
 
-    public ICoroutine StartCoroutine(IEnumerator<ICouroutineSuspendor> routine)
+    private sealed class CoroutineExecutor : ICoroutine, IDisposable
     {
-        throw new NotImplementedException(); //TODO(mtszkarbowiak) Implement.
+        private readonly CoroutinesScript _origin;
+        private readonly IEnumerator<ICouroutineSuspendor> _enumerator;
+        private ICouroutineSuspendor? _currentSuspendor;
+
+        public CoroutineExecutor(CoroutinesScript origin, IEnumerator<ICouroutineSuspendor> enumerator)
+        {
+            _origin = origin;
+            _enumerator = enumerator;
+            _currentSuspendor = _enumerator.Current;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            if (_currentSuspendor != null)
+            {
+                Debug.LogWarning(
+                    $"The coroutine has not ended its execution before disposal. " +
+                    $"Suspendor: {_currentSuspendor} (on {_currentSuspendor?.SuspensionPoints})"
+                );
+            }
+
+            _enumerator.Dispose();
+        }
+
+        public void Stop()
+        {
+            //TODO(mtszkarbowiak) Implement.
+        }
+
+        public void Execute(CoroutineSuspensionPointIndex suspensionPoint)
+        {
+            //TODO(mtszkarbowiak) Implement.
+        }
     }
 
-    public void StopCoroutine(ICoroutine routine)
+    private List<CoroutineExecutor> _executors = new();
+
+    public ICoroutine StartCoroutine(IEnumerator<ICouroutineSuspendor> routine)
     {
-        throw new NotImplementedException(); //TODO(mtszkarbowiak) Implement.
+        var executor = new CoroutineExecutor(this, routine);
+        _executors.Add(executor);
+        return executor;
+    }
+
+    public void StopCoroutine(ref ICoroutine? coroutine)
+    {
+        if (coroutine is not CoroutineExecutor executor)
+        {
+            throw new InvalidOperationException("This is not a coroutine coming from this script!");
+        }
+
+        executor.Stop();
+        executor.Dispose();
+
+        if (!_executors.Remove(executor))
+        {
+            throw new InvalidOperationException("You must not stop a coroutine created by a different script!");
+        }
+
+        coroutine = null;
     }
 
     public void StopAllCoroutines()
     {
-        throw new NotImplementedException(); //TODO(mtszkarbowiak) Implement.
+        while (_executors.Count > 0)
+        {
+            ICoroutine? executor = _executors.Last();
+            StopCoroutine(ref executor);
+        }
     }
 
     private void ExecuteStep()
