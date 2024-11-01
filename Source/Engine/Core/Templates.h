9688c74ca2c0428fd2cc1023b4de2ace7eb92afb
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-11-01 21:10:30 +0100

    Templates TIsAnyOf and TIsIntegral

--- Source/Engine/Core/Templates.h
+++ Source/Engine/Core/Templates.h
@@ -196,6 +196,23 @@ template<typename T> struct TAddConst { typedef const T Type; };
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+template<typename T, typename...>
+struct TIsAnyOf;
+
+template<typename T, typename First, typename... Rest>
+struct TIsAnyOf<T, First, Rest...>
+{
+    enum { Value = TIsTheSame<T, First>::Value || TIsAnyOf<T, Rest...>::Value };
+};
+
+template<typename T>
+struct TIsAnyOf<T>
+{
+    enum { Value = false };
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+
 // Creates a lvalue or rvalue reference type.
 
 namespace THelpers
@@ -384,6 +401,14 @@ inline typename TEnableIf<TAreTypesEqual<T, unsigned int>::Value, T>::Type Rever
 	bits = ((bits & 0x55555555) << 1) | ((bits & 0xaaaaaaaa) >> 1);
 	return bits;
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+struct TIsIntegral
+{
+    enum { Value = TIsAnyOf<T, int8, uint8, int16, uint16, int32, uint32, int64, uint64>::Value };
+};
 
 ////////////////////////////////////////////////////////////////////////////////////
 
