bab757b350df38577ef8f5bc3805b6c02b9197d7
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-11-13 01:38:50 +0100

    SFINAE for allocation move-ability

--- Source/Engine/Core/Collections/CollectionUtils.h
+++ Source/Engine/Core/Collections/CollectionUtils.h
@@ -8,21 +8,33 @@ class CollectionUtils final
 {
 public:
     template<typename T, typename AllocationType>
-    FORCE_INLINE static void MoveToEmpty(
-        typename AllocationType::template Data<T>& to, 
+    FORCE_INLINE
+    static std::enable_if_t<std::is_move_constructible< 
+        typename AllocationType::template Data<T>>::value
+    > MoveToEmpty(
+        typename AllocationType::template Data<T>& to,
         typename AllocationType::template Data<T>& from,
-        const int32 fromCount, 
+        const int32 fromCount,
         const int32 fromCapacity
     )
     {
-        if IF_CONSTEXPR(AllocationType::HasSwap)
-            to.Swap(from);
-        else
-        {
-            to.Allocate(fromCapacity);
-            Memory::MoveItems(to.Get(), from.Get(), fromCount);
-            Memory::DestructItems(from.Get(), fromCount);
-            from.Free();
-        }
+        ::Swap(to, from);
+    }
+
+    template<typename T, typename AllocationType>
+    FORCE_INLINE
+    static std::enable_if_t<!std::is_move_constructible< 
+        typename AllocationType::template Data<T>
+    >::value> MoveToEmpty(
+        typename AllocationType::template Data<T>& to,
+        typename AllocationType::template Data<T>& from,
+        const int32 fromCount,
+        const int32 fromCapacity
+    )
+    {
+        to.Allocate(fromCapacity);
+        Memory::MoveItems(to.Get(), from.Get(), fromCount);
+        Memory::DestructItems(from.Get(), fromCount);
+        from.Free();
     }
 };
