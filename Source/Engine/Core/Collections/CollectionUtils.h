15ea42560ccaceb107284a3ddb8bfa55affb7872
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-11-14 14:20:18 +0100

    * Collections grow function

--- Source/Engine/Core/Collections/CollectionUtils.h
+++ Source/Engine/Core/Collections/CollectionUtils.h
@@ -63,4 +63,33 @@ public:
         //TODO Manually inline this method.
         MoveLinearContentImpl<T, AllocationType>(to, from, fromCount, fromCapacity);
     }
+
+
+    /// <summary>
+    /// Calculates the capacity of the allocation based on the requested count of elements.
+    /// </summary>
+    /// <typeparam name="AllocationType">The type of memory allocator, providing information about allocation capacity limits.</typeparam>
+    /// <param name="count">The requested minimal count of elements to store.</param>
+    template<typename AllocationType>
+    FORCE_INLINE static int32 CalculateCapacity(const int32 count)
+    {
+        static_assert(AllocationType::MinCapacity > 0, "The minimum capacity must be greater than zero.");
+        static_assert(AllocationType::MaxCapacity >= AllocationType::MinCapacity, "The maximum capacity must be greater or equal to the minimum capacity.");
+
+        // 1. Count of zero is a special case. It means that there should not be any allocation at all.
+        // Thus, it should be handled separately and is treated as illegal argument.
+        // 2. The requested capacity must never exceed the maximum capacity of the allocation.
+        ASSERT((0 < count) && (count <= AllocationType::MaxCapacity));
+
+        // By default, the capacity is just doubled.
+        int32 capacity = count;
+        ROUND_UP_TO_POWER_OF_TWO_32(capacity);
+
+        if (capacity < AllocationType::MinCapacity)
+            return AllocationType::MinCapacity;
+        else if (capacity > AllocationType::MaxCapacity)
+            return AllocationType::MaxCapacity; // We already know that count <= MaxCapacity, so this is safe.
+
+        return capacity;
+    }
 };
